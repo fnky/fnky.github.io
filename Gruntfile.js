fe7b0dd2b1412e5d453df1d9ff38d58a1481b1f8
Christian Petersen <humanoidism@gmail.com>
2014-03-25 04:06:54 +0100

    Change commit message for buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,7 +350,7 @@ module.exports = function (grunt) {
                 dir: 'dist',
                 commit: true,
                 push: true,
-                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+                message: 'Built %sourceName% from commit #%sourceCommit% on branch %sourceBranch%'
             },
             pages: {
                 options: {
